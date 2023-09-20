9923cfa7d1cb686e7cc3708b5fa309e6b90acbb9
lesavantdon <144865003+lesavantdon@users.noreply.github.com>
2023-09-20 02:26:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,40 @@ var getComputerChoice = () => {
 };
  console.log(`ComputerChoice: ${getComputerChoice()}`);
 
+
  var determineWinner = function (userChoice, computerChoice) {
-    
- }
+    if (userChoice === computerChoice) {
+        return "the game is a tie";
+    }
+    if (userChoice === 'rock') {
+        if (computerChoice === "paper") {
+            return "the computer won!";
+        } else {
+            return "you won!";
+        }
+    }
+    if (userChoice === "paper") {
+        if (computerChoice === "scissors") {
+            return "the computer won";
+        } else {
+            return "you won!";
+        }
+    }
+    if (userChoice === "scissors") {
+        if (computerChoice === "rock") {
+            return "the computer won";
+        } else {
+            return "you won";
+        }
+    }
+ };
+
+ var playGame = () => {
+    var userChoice = getUserChoice ("rock");
+    var computerChoice = getComputerChoice ();
+    console.log (`you threw: ${userChoice}`);
+    console.log (`computer threw: ${computerChoice}`);
+    console.log(determineWinner(userChoice, computerChoice));
+ };
+ playGame();
+ 
